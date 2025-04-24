afd476a92d7761c5423922a9d77aad498db50424
Aoi109 <136904617+Aoi109@users.noreply.github.com>
2025-04-24 23:02:17 +0900

    Add files via upload

---
+++ character-data.js
@@ -0,0 +1,94 @@
+var characterSet = [
+	{
+		'index': 0,
+		'name': '櫻羽空色',
+		'san': 70,
+		'serifList': [
+			{
+				'serif': 'お～！久しぶりじゃん！！元気にしてた？',
+				'image': 'SORAmain1.png'
+			},
+			{
+				'serif': 'ふんふんふ～～ん♪…あっ！やっほ～！！どうしたの？？',
+				'image': 'SORAmain1.png'
+			},
+		],
+		'tunList': [
+			{
+				'serif': '宇賀地と水熊？何かの研究してんよ～ほんと、死んでも飽きないよね～～',
+				'image': 'SORAmain1.png'
+			},
+			{
+				'serif': '好きな季節？そりゃ夏！！海に行くの好きなんだよね～！！そこまで泳げないんだけど！！！',
+				'image': 'SORAsub1.png'
+			},
+			{
+				'serif': '…あの時、もう少しあたしの手が届いてたら……なんて考えることが無いって言ったら…嘘になるね。',
+				'image': 'SORAsub2.png'
+			},
+			{
+				'serif': '兄貴からの誕プレ？う～～～ん…まぁ……独特のセンスというか…毎年酷かったからなぁ………全部飾ってるけど！',
+				'image': 'SORAsub3.png'
+			},
+			{
+				'serif': '『スケボーに興味がある』？いいじゃん！！あたしが教えるよ～！！こっちでやろ！！',
+				'image': 'SORAsub4.png'
+			},
+			{
+				'serif': '兄貴からの手紙、…死んでから大切なものが増えるとは思わなかったな。海理と仲良く、あたしの分まで長生きしてくれたらいいなぁ。',
+				'image': 'SORAsub5.png'
+			}
+		]
+	},
+	// {
+	// 	'index': 1,
+	// 	'name': '権守 龍玄',
+	// 	'san': 95,
+	// 	'serifList': [
+	// 		{
+	// 			'serif': 'ryugen-a',
+	// 			'image': 'Rmain'
+	// 		}
+	// 	],
+	// 	'tunList': [
+	// 		{
+	// 			'serif': 'ryugen-b',
+	// 			'image': 'Rsub'
+	// 		}
+	// 	]
+	// },
+	// {
+	// 	'index': 2,
+	// 	'name': '彩葉',
+	// 	'san': 45,
+	// 	'serifList': [
+	// 		{
+	// 			'serif': 'iroha-a',
+	// 			'image': 'Imain'
+	// 		}
+	// 	],
+	// 	'tunList': [
+	// 		{
+	// 			'serif': 'iroha-b',
+	// 			'image': 'Isub'
+	// 		}
+	// 	]
+	// },
+	// {
+	// 	'index': 3,
+	// 	'name': '雪之丞',
+	// 	'san': 75,
+	// 	'serifList': [
+	// 		{
+	// 			'serif': 'yukinojo-a',
+	// 			'image': 'Ymain'
+	// 		}
+	// 	],
+	// 	'tunList': [
+	// 		{
+	// 			'serif': 'yukinojo-b',
+	// 			'image': 'Ysub'
+	// 		}
+	// 	]
+	// }
+];
