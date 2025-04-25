8c91558762b500edc1cea733b3845257348f29dc
Aoi109 <136904617+Aoi109@users.noreply.github.com>
2025-04-25 23:06:10 +0900

    Add files via upload

--- character-data.js
+++ character-data.js
@@ -40,23 +40,47 @@ var characterSet = [
 			}
 		]
 	},
-	// {
-	// 	'index': 1,
-	// 	'name': '権守 龍玄',
-	// 	'san': 95,
-	// 	'serifList': [
-	// 		{
-	// 			'serif': 'ryugen-a',
-	// 			'image': 'Rmain'
-	// 		}
-	// 	],
-	// 	'tunList': [
-	// 		{
-	// 			'serif': 'ryugen-b',
-	// 			'image': 'Rsub'
-	// 		}
-	// 	]
-	// },
+	{
+		'index': 1,
+		'name': '反石実玖',
+		'san': 5,
+		'serifList': [
+			{
+				'serif': 'チッ……………',
+				'image': 'MIKUmain1.png'
+			},
+			{
+				'serif': 'テメェと話すことなんかねぇ。失せろ。',
+				'image': 'MIKUmain1.png'
+			}
+		],
+		'tunList': [
+			{
+				'serif': 'で、いつになったらオレは転生できンだ？やる気あンのか？なぁ？？',
+				'image': 'MIKUmain1.png'
+			},
+			{
+				'serif': 'バケモンに追いかけられて死んで、またバケモンに追いかけられるとはなぁ。ま、二度も同じヘマはしねぇけどな！！ハハッ！！',
+				'image': 'MIKUsub1.png'
+			},
+			{
+				'serif': 'ドリームランドに行く前……ア"ー…生きてる時な？変な夢を見た気がすンだけどありゃ何だったんだろうな。',
+				'image': 'MIKUsub2.png'
+			},
+			{
+				'serif': 'クソつまんねぇ人生だったけど、…まァ最後の最後は楽しかったのかもな。生に執着はしてねぇけど、生きてりゃオレの人生も変わってたのかもしンねぇな。',
+				'image': 'MIKUsub3.png'
+			},
+			{
+				'serif': 'ア"？？…何様だ？？？オレに命令すンなんて100億年早ぇわ。出直せ。',
+				'image': 'MIKUsub4.png'
+			},
+			{
+				'serif': '好きな季節？前も聞いただろ。何回も聞くンじゃねぇ、クソが。',
+				'image': 'MIKUsub5.png'
+			},
+		]
+	},
 	// {
 	// 	'index': 2,
 	// 	'name': '彩葉',
